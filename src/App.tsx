b79dcde9fa088fd11bf89310760e8c480eac1b56
Limarev Ivan <ivantaketo@gmail.com>
2024-06-25 14:57:10 +0600

    create Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import "./App.css";
 import {NavLink, Route, Routes} from "react-router-dom";
 import Home from "./containers/Home/Home";
-import AboutUs from "./containers/AboutUs/AboutUs";
+import About from "./containers/About/About";
 import Contacts from "./containers/Contacts/Contacts";
+import logo from "./assets/img/logo.png";
 
 const App = () => {
     return (
         <>
-            <header>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <NavLink to="/" className="navbar-brand">Capybaras</NavLink>
+            <header className="mb-3">
+                <nav className="navbar navbar-expand-lg p-0 bg-warning" data-bs-theme="dark">
+                    <div className="container-xl">
+                        <NavLink to="/" className="navbar-brand"><img src={logo} style={{width: 100}} alt="Capybaras"/></NavLink>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                                 aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
+                            <ul className="navbar-nav ms-auto gap-4 fs-4">
                                 <li className="nav-item">
                                     <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to="/about-us" className="nav-link">About us</NavLink>
+                                    <NavLink to="/about" className="nav-link">About capybaras</NavLink>
                                 </li>
                                 <li className="nav-item">
                                     <NavLink to="/contacts" className="nav-link">Contacts</NavLink>
@@ -32,15 +32,19 @@ const App = () => {
                     </div>
                 </nav>
             </header>
-            <main className="main">
+            <main className="main container-xl">
                <Routes>
                    <Route path="/" element={<Home />}/>
-                   <Route path="/about-us" element={<AboutUs />}/>
+                   <Route path="/about" element={<About />}/>
                    <Route path="/contacts" element={<Contacts />}/>
                    <Route path="*" element={<h1>Page not a Found!</h1>}/>
                </Routes>
             </main>
-            <footer></footer>
+            <footer className="navbar navbar-expand-lg p-0 bg-warning">
+                <div className="container-xl py-4 d-flex justify-content-center align-items-center">
+                    <p className="text-white text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda, debitis esse ex expedita laboriosam modi nihil officiis porro vero!</p>
+                </div>
+            </footer>
         </>
     );
 };
